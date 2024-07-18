5aed471a0a0dad1379b1f1527002ade0419a7364
sw326 <sungwoo326@gmail.com>
2024-07-18 14:41:38 +0900

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,65 +1,66 @@
-import axios from 'axios';
-import { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
-    const { login } = useContext(AuthContext);
-    const nav = useNavigate();
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+  const nav = useNavigate();
 
-    const loginSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post('http://3.38.191.164/login', {
-                id,
-                password,
-            });
-            console.log(response);
-            if (response.status === 201) {
-                //토큰 저장하기 > 로컬스토리지에
-                login(data.accessToken);
-                nav('/');
-            } else {
-                alert('Login failed');
-            }
-        } catch (error) {
-            console.error('Login error:', error);
-            alert('Login failed');
-        }
-    };
+  const loginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://3.38.191.164/login", {
+        id,
+        password,
+      });
+        console.log("response", response);
+      if (response.status === 201) {
+        //토큰 저장하기 > 로컬스토리지에
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={loginSubmit}>
-                <p>ID</p>
-                <input
-                    type="text"
-                    value={id}
-                    onChange={(e) => {
-                        setId(e.target.value);
-                    }}
-                    placeholder="ID"
-                />
-                <p>PW</p>
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                    placeholder="PW"
-                />
-                <br />
-                <button type="submit">로그인</button>
-                <br />
+        login(response.data.token);
+        nav("/");
+      } else {
+        alert("Login failed");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      alert("Login failed");
+    }
+  };
 
-                <button onClick={() => nav(`/register`)}>회원가입</button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={loginSubmit}>
+        <p>ID</p>
+        <input
+          type="text"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+          placeholder="ID"
+        />
+        <p>PW</p>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="PW"
+        />
+        <br />
+        <button type="submit">로그인</button>
+        <br />
+
+        <button onClick={() => nav(`/register`)}>회원가입</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
